5f941371c631a028f0ad26b5fa91f3006e53b9e7
Gzim <gzimasanni@gmail.com>
2021-03-23 15:06:05 +0100

    Add functionality, now page displays profiles

--- ui.js
+++ ui.js
@@ -9,13 +9,26 @@ class UI {
       <div class="row">
         <div class="col-md-3">
         <img class="img-fluid mb-2" src="${user.avatar_url}">
-        <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View Profile</a>
+        <a href="${user.html_url}" target="_blank" class="btn btn-primary mb-4 btn-block">View Profile</a>
         </div>
         <div class="col-md-9">
-        <span class="badge badge-primary">Repositories</span>
+        <span class="badge badge-primary">Repositories: ${user.public_repos}</span>
+        <span class="badge badge-secondary">Public gists: ${user.public_gists}</span>
+        <span class="badge badge-sucess">Followers: ${user.followers}</span>
+        <span class="badge badge-info">Following: ${user.following}</span>
+        <br><br>
+
+        <ul class="line-group">
+          <li class="list-group-item">Company: ${user.company}</li>
+          <li class="list-group-item">Website/blog: ${user.blog}</li>
+          <li class="list-group-item">Location: ${user.location}</li>
+          <li class="list-group-item">Member since: ${user.created_at}</li>
+        </ul
         </div>
       </div>
     </div>
+    <h3>Public Repositories:</h3>
+    <div id="repos"></div>
     `
   }
 }
